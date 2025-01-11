cef6e8fd134fee69a7420570fd24c3aae74a58b5
sirajudeen <inayathullahbasha79@gmail.com>
2025-01-11 11:56:17 +0000

    "client 4 hrs completed"

--- client/context/globalContext.js
+++ client/context/globalContext.js
@@ -17,6 +17,20 @@ export const GlobalContextProvider = ({children}) =>{
     const [userProfile,setUserProfile] = useState({});
     const [loading,setLoading] = useState(false);
 
+    const [jobTitle, setJobTitle] = useState("");
+    const [jobDescription, setJobDescription] = useState("");
+    const [salary, setSalary] = useState(0);
+    const [activeEmploymentTypes, setActiveEmploymentTypes] = useState([]);
+    const [salaryType, setSalaryType] = useState("Year");
+    const [negotiable, setNegotiable] = useState(false);
+    const [tags, setTags] = useState([]);
+    const [skills, setSkills] = useState([]);
+    const [location, setLocation] = useState({
+      country: "",
+      city: "",
+      address: "",
+    });
+
     useEffect(() => {
         const checkAuth = async () =>{
             setLoading(true);
@@ -53,6 +67,37 @@ export const GlobalContextProvider = ({children}) =>{
                 console.log("error getting user profile",error);
             }
         };
+
+          // handle input change
+  const handleTitleChange = (e) => {
+    setJobTitle(e.target.value.trimStart());
+  };
+
+  const handleDescriptionChange = (e) => {
+    setJobDescription(e.target.value.trimStart());
+  };
+
+  const handleSalaryChange = (e) => {
+    setSalary(e.target.value);
+  };
+
+  const resetJobForm = () => {
+    setJobTitle("");
+    setJobDescription("");
+    setSalary(0);
+    setActiveEmploymentTypes([]);
+    setSalaryType("Year");
+    setNegotiable(false);
+    setTags([]);
+    setSkills([]);
+    setLocation({
+      country: "",
+      city: "",
+      address: "",
+    });
+  };
+
+
         useEffect(() => {
             if(isAuthenticated && auth0User){
                 getUserProfile(auth0User.sub);
@@ -64,12 +109,31 @@ export const GlobalContextProvider = ({children}) =>{
     return(
         <GlobalContext.Provider value={
             {
-                isAuthenticated,
-                setIsAuthenticated,
-                auth0User,
-                userProfile,
-                getUserProfile,
-                loading,
+                  isAuthenticated,
+        auth0User,
+        userProfile,
+        getUserProfile,
+        loading,
+        jobTitle,
+        jobDescription,
+        salary,
+        activeEmploymentTypes,
+        salaryType,
+        negotiable,
+        tags,
+        skills,
+        location,
+        handleTitleChange,
+        handleDescriptionChange,
+        handleSalaryChange,
+        setActiveEmploymentTypes,
+        setJobDescription,
+        setSalaryType,
+        setNegotiable,
+        setTags,
+        setSkills,
+        setLocation,
+        resetJobForm,
             }
         }>
             {children}
--- server/server.js
+++ server/server.js
@@ -19,6 +19,12 @@ const config = {
   baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL,
+  routes:{
+    postLogoutRedirect: process.env.CLIENT_URL,
+    callback: "/callback",
+    logout: "/logout",
+    login: "/login",
+  }
   // Add state parameter to avoid BadRequestError
  
 };
