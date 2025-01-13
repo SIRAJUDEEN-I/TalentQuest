1726f12eebc5cbcd57bc8c620305a769d8f065a8
sirajudeen <sirajudeen4300@gmail.com>
2025-01-13 19:56:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,10 +30,15 @@ const config = {
 };
 
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-}));
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["set-cookie"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
