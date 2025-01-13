2d5be2e0bfa7fc8d6aa57916018ffed84bd02e35
SIRAJUDEEN I <124486027+Sirajudeen-I-BCA@users.noreply.github.com>
2025-01-13 12:33:15 +0000

    d

---
+++ client/components/FindWorkComponent.tsx
@@ -0,0 +1,9 @@
+import { useSearchParams } from 'next/navigation';
+// ...existing code...
+
+const FindWorkComponent = () => {
+  const searchParams = useSearchParams();
+  // ...existing code...
+};
+
+export default FindWorkComponent;
