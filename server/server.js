e4608e4f07d9f4cbb6557c7b5f612070afd05d4e
sirajudeen <sirajudeen4300@gmail.com>
2025-08-30 21:03:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -111,7 +111,7 @@ const server = async () => {
   try {
     await connect();
 
-    app.listen(process.env.PORT, () => {
+    app.listen(8000, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
   } catch (error) {
